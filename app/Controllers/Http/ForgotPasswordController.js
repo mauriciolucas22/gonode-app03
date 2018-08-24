6016421f3ed73488574b90615faa223149afd87e
Mauricio Lucas <mauricio.nq@hotmail.com>
2018-08-24 14:22:58 -0300

    store for ForgotPassword ok

--- app/Controllers/Http/ForgotPasswordController.js
+++ app/Controllers/Http/ForgotPasswordController.js
@@ -1,7 +1,19 @@
 'use strict'
 
+const crypto = require('crypto')
+const User = use('App/Models/User')
+
 class ForgotPasswordController {
-  async store () {}
+  async store ({ request }) {
+    const email = request.input('email') // busca 1 campo
+    const user = await User.findBy('email', email) // busca 1 registro por outra coluna=email
+
+    // token com 10 bytes e converte para hexadecimal
+    user.token = crypto.randomBytes(10).toString('hex')
+    user.token_created_at = new Date()
+
+    await user.save()
+  }
 }
 
 module.exports = ForgotPasswordController
