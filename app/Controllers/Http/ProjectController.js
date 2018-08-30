f96985f5bd15753c1fefbe28f6636bcfda2ae2ca
Mauricio Lucas <mauricio.nq@hotmail.com>
2018-08-30 16:28:03 -0300

    pagination ok

--- app/Controllers/Http/ProjectController.js
+++ app/Controllers/Http/ProjectController.js
@@ -10,8 +10,12 @@ class ProjectController {
    * Show a list of all projects.
    * GET projects
    */
-  async index ({ request, response, view }) {
-    const projects = await Project.query().with('user').fetch()
+  async index ({ request }) {
+    const { page } = request.get()
+
+    const projects = await Project.query()
+      .with('user')
+      .paginate(page)
 
     return projects
   }
